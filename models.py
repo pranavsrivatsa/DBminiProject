7e1fbe677b28f5d313185c258f2f3db3971f327b
Devajji <devajjir@gmail.com>
2018-10-16 17:26:41 +0530

    schema and erd

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/apms'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 class Accounts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,3 +16,13 @@ class Accounts(db.Model):
 
     def __repr__(self):
         return self.password
+
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    age = db.Column(db.String(128), unique=True, nullable=False)
+    type = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return self.type
