77b317c1039509e8fb418f0bf26b77bc50d54592
Crystal_Space <pranav.srivatsa27@gmail.com>
2018-11-17 19:12:32 +0530

    fine

--- models.py
+++ models.py
@@ -1,51 +1,25 @@
-import json
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-<<<<<<< HEAD
 from datetime import datetime
-=======
-from sqlalchemy.ext.declarative import DeclarativeMeta
->>>>>>> Removed Json
 
-
-'''
+"""
 >python3 models.py db --help
 >python3 models.py db init
 >python3 models.py db migrate
 >python3 models.py db upgrade
-'''
+"""
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/apms'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app,db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-<<<<<<< HEAD
-=======
-
-# class AlchemyEncoder(json.JSONEncoder):
-#     def default(self, obj):
-#         if isinstance(obj.__class__, DeclarativeMeta):
-#             fields = {}
-#             for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-#                 data = obj.__getattribute__(field)
-#                 try:
-#                     # this will fail on non-encodable values, like other classes
-#                     json.dumps(data)
-#                     fields[field] = data
-#                 except TypeError:
-#                     fields[field] = None
-#             return fields
-#         return json.JSONEncoder.default(self, obj)
-
-
->>>>>>> Removed Json
 class Account(db.Model):
     __tablename__ = 'account'
     id = db.Column(db.Integer, primary_key=True)
@@ -56,34 +30,18 @@ class Account(db.Model):
     def __repr__(self):
         return self.password
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> Removed Json
 CustomerRides_Table = db.Table('customerrides',
-                               db.Column('customerId', db.Integer, db.ForeignKey(
-                                   'customer.id'), primary_key=False),
-                               db.Column('rideId', db.Integer, db.ForeignKey(
-                                   'ride.id'), primary_key=False)
-                               )
+    db.Column('customerId',db.Integer, db.ForeignKey('customer.id'),primary_key=False),
+    db.Column('rideId',db.Integer, db.ForeignKey('ride.id'), primary_key=False)
+    )
 
-
->>>>>>> customerrides primary key as false
 class Customer(db.Model):
     __tablename__ = 'customer'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     type = db.Column(db.String(128), nullable=False)
-<<<<<<< HEAD
     rides = db.relationship('CustomerRidesLink')
-=======
-    rides = db.relationship('Ride', secondary=CustomerRides_Table, lazy='subquery',
-                            backref=db.backref('customers', lazy=True))
-
->>>>>>> Removed Json
 
 class Ride(db.Model):
     __tablename__ = 'ride'
@@ -94,7 +52,6 @@ class Ride(db.Model):
     def __repr__(self):
         return self.price
 
-<<<<<<< HEAD
 class CustomerRidesLink(db.Model):
     __tablename__ = 'customerrides'
     id = db.Column(db.Integer, primary_key=True)
@@ -106,8 +63,4 @@ class CustomerRidesLink(db.Model):
 
 
 if __name__=='__main__':
-=======
-
-if __name__ =='__main__':
->>>>>>> Removed Json
     manager.run()
