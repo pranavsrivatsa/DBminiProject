684275a2ebd3e9d5008a14074ffbdebd09fc246c
Crystal_Space <pranav.srivatsa27@gmail.com>
2018-11-23 16:43:51 +0530

    proc

---
+++ dayrevenue.sql
@@ -0,0 +1,75 @@
+CREATE OR REPLACE FUNCTION dayfill () 
+RETURNS TRIGGER AS $$ 
+DECLARE   
+    Carouselprice INTEGER := 0 ;
+    Darkrideprice INTEGER := 0 ;
+    Droptowerprice INTEGER := 0 ;
+    Ferriswheelprice INTEGER := 0 ;
+    Gyrotowerprice INTEGER := 0 ;
+    Rollercoasterprice INTEGER := 0 ;
+    Waterrideprice INTEGER := 0 ;
+    SpiralSlideprice INTEGER := 0 ;
+    Circusprice INTEGER := 0 ;
+    Gravitronprice INTEGER := 0 ;
+    Carouselprice1 INTEGER := 0 ;
+    Darkrideprice1 INTEGER := 0 ;
+    Droptowerprice1 INTEGER := 0 ;
+    Ferriswheelprice1 INTEGER := 0 ;
+    Gyrotowerprice1 INTEGER := 0 ;
+    Rollercoasterprice1 INTEGER := 0 ;
+    Waterrideprice1 INTEGER := 0 ;
+    SpiralSlideprice1 INTEGER := 0 ;
+    Circusprice1 INTEGER := 0 ;
+    Gravitronprice1 INTEGER := 0 ;
+BEGIN 
+    Carouselprice := count(*) from customerrides where "rideId" = 1;
+    Carouselprice1 := r.price from ride r where id = 1;
+    Carouselprice := Carouselprice * Carouselprice1;
+    
+    Darkrideprice := count(*) from customerrides where "rideId" = 2;
+    Darkrideprice1 := r.price from ride r where id = 2;
+    Darkrideprice := Darkrideprice * Darkrideprice1;
+    
+    Droptowerprice := count(*) from customerrides where "rideId" = 3;
+    Droptowerprice1 := r.price from ride r where id = 3;
+    Droptowerprice := Droptowerprice * Droptowerprice1;
+    
+    Ferriswheelprice := count(*) from customerrides where "rideId" = 4;
+    Ferriswheelprice1 := r.price from ride r where id = 4;
+    Ferriswheelprice := Ferriswheelprice * Ferriswheelprice1;
+    
+    Gyrotowerprice := count(*) from customerrides where "rideId" = 5;
+    Gyrotowerprice1 := r.price from ride r where id = 5;
+    Gyrotowerprice := Gyrotowerprice * Gyrotowerprice1;
+    
+    Rollercoasterprice := count(*) from customerrides where "rideId" = 6;
+    Rollercoasterprice1 := r.price from ride r where id = 6;
+    Rollercoasterprice := Rollercoasterprice * Rollercoasterprice1;
+    
+    Waterrideprice := count(*) from customerrides where "rideId" = 7;
+    Waterrideprice1 := r.price from ride r where id = 7;
+    Waterrideprice := Waterrideprice * Waterrideprice1;
+    
+    SpiralSlideprice := count(*) from customerrides where "rideId" = 8;
+    SpiralSlideprice1 := r.price from ride r where id = 8;
+    SpiralSlideprice := SpiralSlideprice * SpiralSlideprice1;
+    
+    Circusprice := count(*) from customerrides where "rideId" = 9;
+    Circusprice1 := r.price from ride r where id = 9;
+    Circusprice := Circusprice * Circusprice1;
+   
+    Gravitronprice := count(*) from customerrides where "rideId" = 10;
+    Gravitronprice1 := r.price from ride r where id = 10;
+    Gravitronprice := Gravitronprice * Gravitronprice1;
+    
+    insert into dayrev("time", "Carousel", "Darkride", "Droptower", "Ferriswheel", "Gyrotower", "Rollercoaster", "Waterride", "SpiralSlide", "Circus", "Gravitron") values (new.time::timestamp::date, Carouselprice, Darkrideprice, Droptowerprice, Ferriswheelprice, Gyrotowerprice, Rollercoasterprice, Waterrideprice, SpiralSlideprice, Circusprice, Gravitronprice);
+    RETURN new;
+END;$$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER update_dayrev
+    AFTER INSERT ON customerrides
+    FOR EACH ROW
+    --WHEN (new.time::timestamp::date::day = 28)
+    WHEN ( extract (hour from new.time ) = 18)
+    EXECUTE PROCEDURE dayfill();
--- populateDB.py
+++ populateDB.py
@@ -5,7 +5,7 @@ import models
 import random
 import datetime
 import time
-
+from datetime import datetime, timedelta
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/apms'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -19,23 +19,42 @@ def change():
 fake = Faker()
 
 def populateCustomerRides():
-    a = 1
+    global a
     customerList = models.Customer.query.all()
     rideList = models.Ride.query.all()
-    tim = datetime.datetime.now()
+    tim = datetime(2018,12,27,10,0)
     while True:
         if a is 0:
             break
         customer = random.choice(customerList)
         ride = random.choice(rideList)
-        newTime = tim + datetime.timedelta(0,60) # days, seconds, then other fields.
-        tim = newTime
-        time.sleep(5)
-        customerride = models.CustomerRidesLink(customerId=customer.id,rideId=ride.id,time=tim)
+        newTime = tim + timedelta(0,1800) # days, seconds, then other fields.
+        if newTime.hour == 18:
+            tim = datetime(newTime.year,newTime.month,newTime.day+1,10,00)
+            if newTime.day == 28:
+                if newTime.month < 12:
+                    tim = datetime(newTime.year,newTime.month+1,newTime.day,10,00)
+                if newTime.month == 12:
+                    tim = datetime(newTime.year+1,1,1,10,00)
+        else:
+            tim = newTime
+        customerride = models.CustomerRidesLink(customerId=customer.id,rideId=ride.id,time=newTime)
         db.session.add(customerride)
+        db.session.commit()
+        time.sleep(3)
+
+def populateRide():
+    ride_list = ['Carousel','Darkride','Droptower','Ferriswheel','Gyrotower','Rollercoaster','Waterride','SpiralSlide','Circus','Gravitron']
+    for ride in ride_list:
+        name = ride
+        price = (random.randint(20,30)) * 10
+        maintenance_cost = (random.randint(20,30)) * 100
+        rideDetails = models.Ride(name=name,price=price,maintenance_cost=maintenance_cost)
+        db.session.add(rideDetails)
         db.session.commit()
 
 def populateCustomer():
+    populateRide()
     count = 100
     type_list = ['normal','student']
     age10 = list(range(5,10))
@@ -59,15 +78,5 @@ def populateCustomer():
         db.session.add(customer)
         db.session.commit()
         count -= 1
-
-def populateRide():
-    ride_list = ['Carousel','Darkride','Droptower','Ferriswheel','Gyrotower','Rollercoaster','Waterride','SpiralSlide','Circus','Gravitron']
-    for ride in ride_list:
-        name = ride
-        price = (random.randint(20,30)) * 10
-        maintenance_cost = (random.randint(20,30)) * 100
-        rideDetails = models.Ride(name=name,price=price,maintenance_cost=maintenance_cost)
-        db.session.add(rideDetails)
-        db.session.commit()
 
 a = 1
